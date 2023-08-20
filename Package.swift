524477cb107778ab79ff323b57c783f142c53f6c
Tomas Harkema <tomas@harkema.io>
2023-08-20 04:21:15 +0200

    updates

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MapboxDirections",
     platforms: [
-        .macOS(.v10_14), .iOS(.v12), .watchOS(.v5), .tvOS(.v12)
+        .macOS(.v10_14), .iOS(.v14), .watchOS(.v5), .tvOS(.v14)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -21,7 +21,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/raphaelmor/Polyline.git", from: "5.0.2"),
-        .package(name: "Turf", url: "https://github.com/mapbox/turf-swift.git", from: "2.6.1"),
+        .package(url: "https://github.com/mapbox/turf-swift.git", from: "2.6.1"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs", from: "9.1.0")
     ],
@@ -30,12 +30,17 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MapboxDirections",
-            dependencies: ["Polyline", "Turf"],
+            dependencies: [
+                .product(name: "Polyline", package: "Polyline"),
+                .product(name: "Turf", package: "turf-swift")
+            ],
             exclude: ["Info.plist"]),
         .testTarget(
             name: "MapboxDirectionsTests",
             dependencies: [
                 "MapboxDirections",
+                .product(name: "Polyline", package: "Polyline"),
+                .product(name: "Turf", package: "turf-swift"),
                 .product(name:  "OHHTTPStubsSwift", package: "OHHTTPStubs", condition: .when(platforms: [.macOS, .iOS, .tvOS, .watchOS]))
             ],
             exclude: ["Info.plist"],
